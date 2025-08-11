1c600a412192bff322bd980191b9bf9a2623e9b1
dawangegovindamaruti <govindadawangepatil@gmail.com>
2025-08-11 23:33:33 +0530

    change

--- src/app.js
+++ src/app.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const JOBS = require('./jobs');
+const mustacheExpress = require('mustache-express');
 
+console.log(JOBS);
 
 app.use(express.static(path.join(__dirname , 'public')));
 
+
+
+app.engine('mustache', mustacheExpress());
+app.set('view engine','mustache');
+app.set('views', path.join(__dirname,'pages'));
+
+
+
+// app.set('views',path.join(__dirname,'public'))
 app.get('/', (req, res) => {
   // res.send('hellow world !');
-   res.sendFile(path.join(__dirname, 'pages/index.html'));
-}) 
+  //  res.sendFile(path.join(__dirname, 'pages/index.html'));
+  res.render('index',{jobs:JOBS});
+});
+
+app.get('/jobs/:id',(req, res) =>{
+  console.log('req.params', req.params);
+  const id = req.params.id;
+  const matchedJob = JOBS.find(job => job.id.toString() == id);
+  res.render('job', {job : matchedJob});
+})
 
 
 
---
+++ src/jobs.js
@@ -0,0 +1,34 @@
+const JOBS = [
+     {
+      id : 1,
+      title: 'frontend developer',
+       location:'bengluru, india',
+       salary : '$12,00,000',
+       posted:'mar 3, 2025'
+     },
+      {
+      id : 2,
+      title: 'full stack developer',
+       location:'new delhi, india',
+       salary : '$15,00,000',
+       posted:'feb 1, 2025'
+     },
+      {
+      id : 3,
+      title: 'data scientist',
+       location:'san francisco, USA',
+       salary : '$175,000',
+       posted:'Dec 22, 2025'
+     },
+      {
+      id : 4,
+      title: 'ML engineer',
+       location:'Remote',
+       salary : '$80,000',
+       posted:'sep 19, 2025'
+     },
+
+]
+
+module.exports = JOBS;
+    
